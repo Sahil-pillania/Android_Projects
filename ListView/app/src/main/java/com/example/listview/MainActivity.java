385ea7983d1d61b0eccf135519778ee31607e6f8
Sahil-pillania <sahilpillania88@gmail.com>
2022-05-08 22:08:08 +0530

    Committed

--- ListView/app/src/main/java/com/example/listview/MainActivity.java
+++ ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
+    String[] arr = {"This is ","Item","Item 2", "Item 3", "Item 4","This is ","Item","Item 2", "Item 3", "Item 4","This is ","Item","Item 2", "Item 3", "Item 4","This is ","Item","Item 2", "Item 3", "Item 4"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> ad = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr);
+        listView.setAdapter(ad);
+
     }
 }
