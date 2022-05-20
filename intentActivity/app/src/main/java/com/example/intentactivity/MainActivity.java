c33ed5d80b19bc6bbd55f8ff5230f13f50092ff5
Sahil-pillania <sahilpillania88@gmail.com>
2022-05-20 12:13:35 +0530

    Intent inprocess

--- intentActivity/app/src/main/java/com/example/intentactivity/MainActivity.java
+++ intentActivity/app/src/main/java/com/example/intentactivity/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.intentactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText data;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        data = findViewById(R.id.data);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, activity_next.class);
+                intent.putExtra("name", data.getText().toString());
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
