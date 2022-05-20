f0c880595604edeaf65bd2d856e3815b3729dfc4
Sahil-pillania <sahilpillania88@gmail.com>
2022-05-20 20:51:40 +0530

    Intent activity_next

--- intentActivity/app/src/main/java/com/example/intentactivity/activity_next.java
+++ intentActivity/app/src/main/java/com/example/intentactivity/activity_next.java
@@ -10,6 +10,6 @@ public class activity_next extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-        
+
     }
 }
