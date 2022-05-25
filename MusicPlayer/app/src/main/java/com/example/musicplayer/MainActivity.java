862c76bb4cfb418bd4c5bf8c248cdab197974a8e
Sahil-pillania <sahilpillania88@gmail.com>
2022-05-25 20:31:38 +0530

    Music player design

--- MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.musicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button play, pause, stop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
