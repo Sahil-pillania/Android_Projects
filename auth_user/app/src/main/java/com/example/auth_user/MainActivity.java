a45f008fe8c567765309a5186b1d2735c5d700c4
Sahil pillania <sahilpillania88@gmail.com>
2022-06-24 12:51:00 +0530

    user auth

--- auth_user/app/src/main/java/com/example/auth_user/MainActivity.java
+++ auth_user/app/src/main/java/com/example/auth_user/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.auth_user;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText name, password;
@@ -19,12 +21,31 @@ public class MainActivity extends AppCompatActivity {
         name = findViewById(R.id.name);
         password = findViewById(R.id.password);
         button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                name
-            }
-        });
+     name.setOnTouchListener(new View.OnTouchListener() {
+         @Override
+         public boolean onTouch(View view, MotionEvent motionEvent) {
+             textView.setText("");
+             return false;
+         }
+     });
+
+     button.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             String uname = name.getText().toString();
+             String pass = password.getText().toString();
+
+             if((uname.equals("Sahil")) && (pass.equals("password"))){
+                 Toast.makeText(MainActivity.this, "Valid User", Toast.LENGTH_SHORT).show();
+                 textView.setText("Valid User");
+             }
+             else{
+                 Toast.makeText(MainActivity.this, "Invalid User", Toast.LENGTH_SHORT).show();
+                 textView.setText("Invalid User");
+             }
+         }
+     });
     }
 }
