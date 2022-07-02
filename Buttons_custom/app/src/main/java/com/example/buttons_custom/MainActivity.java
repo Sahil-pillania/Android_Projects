0e84134fa0e75a818cef3727c918acbcf0aba0cf
Sahil pillania <sahilpillania88@gmail.com>
2022-07-02 11:25:08 +0530

    custom button background

--- Buttons_custom/app/src/main/java/com/example/buttons_custom/MainActivity.java
+++ Buttons_custom/app/src/main/java/com/example/buttons_custom/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.buttons_custom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button, button2, button3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button.setOnClickListener ({
+            button.isSelected = !button.isSelected
+        })
+        };
     }
 }
